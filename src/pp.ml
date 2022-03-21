4f0c22ccbd79ca637760adb64a7d3db47e6baef9
Dario Pinto <laradiopotin@gmail.com>
2022-03-21 15:46:07 +0100

    fix typo

--- src/pp.ml
+++ src/pp.ml
@@ -1,14 +1,14 @@
 open Ast
 
 let literal fmt = function
-  | Unit -> Format.fprintf fmt "()"
-  | Bool b -> if b then Format.fprintf fmt "true" else Format.fprintf fmt "false"
+  | Unit -> Format.fprintf fmt "Unit"
+  | Bool b -> if b then Format.fprintf fmt "True" else Format.fprintf fmt "False"
   | Int i -> Format.fprintf fmt "%d" i
 
 let rec expression fmt = function
   | Literal l -> literal fmt l
   | Var s -> Format.fprintf fmt "%s" s
   | Bind (s, exp1, exp2) -> Format.fprintf fmt "let %s = %a in %a" s expression exp1 expression exp2
-  | Abstract (s, exp) -> Format.fprintf fmt "(fun %s -> %a)"
-  | Apply (exp1, exp2) -> Format.fprintf fmt "%a %a" expression exp1 expression exp2
+  | Abstract (s, exp) -> Format.fprintf fmt "(fun %s -> %a"
+  | Apply (exp1, exp2) -> Format.fprintf fmt "(%a) (%a)" expression exp1 expression exp2
   | If (exp1, exp2, exp3) -> Format.fprintf fmt "if %a then %a else %a end" expression exp1 expression exp2 expression exp3
