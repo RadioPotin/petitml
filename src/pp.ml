627637869fa1a35670560ac20c5cb009c7fbe780
Dario Pinto <laradiopotin@gmail.com>
2022-03-21 15:41:11 +0100

    add some printing

---
+++ src/pp.ml
@@ -0,0 +1,14 @@
+open Ast
+
+let literal fmt = function
+  | Unit -> Format.fprintf fmt "()"
+  | Bool b -> if b then Format.fprintf fmt "true" else Format.fprintf fmt "false"
+  | Int i -> Format.fprintf fmt "%d" i
+
+let rec expression fmt = function
+  | Literal l -> literal fmt l
+  | Var s -> Format.fprintf fmt "%s" s
+  | Bind (s, exp1, exp2) -> Format.fprintf fmt "let %s = %a in %a" s expression exp1 expression exp2
+  | Abstract (s, exp) -> Format.fprintf fmt "%s %a"
+  | Apply (s, exp) -> Format.fprintf fmt "%s %a" s expression exp
+  | If (exp1, exp2, exp3) -> Format.fprintf fmt "if %a then %a else %a" expression exp1 expression exp2 expression exp3
