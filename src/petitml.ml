88679c4ae104c552051e2890bd2c99b4d35e9ee6
Dario Pinto <laradiopotin@gmail.com>
2022-03-29 15:42:07 +0200

    add new example

--- src/petitml.ml
+++ src/petitml.ml
@@ -2,7 +2,7 @@ open Petitml.Parse
 
 let () =
   let file = Sys.argv.(1) in
-  try from_file file with
+  match from_file file with
   | Ok prog ->
       let fmt = Format.std_formatter in
       Petitml.Pp.expression fmt prog
