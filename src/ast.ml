3a363d25aa168fc6daae6c2e28c5870b0c9cd555
Dario Pinto <laradiopotin@gmail.com>
2022-03-31 15:22:01 +0200

    first shot at scope analysis

--- src/ast.ml
+++ src/ast.ml
@@ -12,27 +12,3 @@ type expression =
   | If of expression * expression * expression
 
 type program = expression
-
-module Variable : sig
-  type t
-
-  val fresh : string -> t
-end = struct
-  type t = string * int
-
-  let counter = ref 0
-
-  let fresh name =
-    incr counter;
-    (name, !counter)
-end
-
-type variable = Variable.t
-
-type varexpression =
-  | Vliteral of literal
-  | Vvar of variable
-  | Vbind of variable * varexpression * varexpression
-  | Vabstract of variable * varexpression
-  | Vapply of varexpression * varexpression
-  | Vif of varexpression * varexpression * varexpression
