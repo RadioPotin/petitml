6195e92e5b69cd37d2c2578f5ea9297f11ee1130
Dario Pinto <laradiopotin@gmail.com>
2022-03-31 17:36:49 +0200

    kinda fixed it

--- src/scope_analysis.ml
+++ src/scope_analysis.ml
@@ -3,12 +3,12 @@ module Env = Map.Make (String)
 let scope_analysis (ast : Ast.program) : Ast.program =
   let open Ast in
   let make_fresh =
-    let seen = Hashtbl.create 32
-    in fun id ->
+    let seen = Hashtbl.create 32 in
+    fun id ->
       match Hashtbl.find_opt seen id with
       | None ->
         Hashtbl.add seen id 0;
-        id ^ "0"
+        id
       | Some nb ->
         let newid = id ^ string_of_int nb in
         Hashtbl.add seen id (nb + 1);
@@ -19,16 +19,16 @@ let scope_analysis (ast : Ast.program) : Ast.program =
     | Var s -> begin
       match Env.find_opt s env with
       | None -> Var (make_fresh s)
-      | Some id -> Var (id)
+      | Some id -> Var id
     end
     | Bind (s, exp1, exp2) ->
-        let s' = make_fresh s in
-        let env' = Env.add s s' env in
-        Bind (s', aux env exp1, aux env' exp2)
+      let s' = make_fresh s in
+      let env' = Env.add s s' env in
+      Bind (s', aux env exp1, aux env' exp2)
     | Abstract (s, exp) ->
       let s' = make_fresh s in
-        let env' = Env.add s s' env in
-        Abstract (s', aux env' exp)
+      let env' = Env.add s s' env in
+      Abstract (s', aux env' exp)
     | Apply (exp1, exp2) -> Apply (aux env exp1, aux env exp2)
     | If (exp1, exp2, exp3) -> If (aux env exp1, aux env exp2, aux env exp3)
   in
